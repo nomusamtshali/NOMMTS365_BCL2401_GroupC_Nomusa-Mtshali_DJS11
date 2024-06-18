853965536fde659e58ae0727c15cb54b13fce7c7
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-06-18 09:13:09 +0200

    created a Navbar component that'll be used to navigate to different pages of the app

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,13 @@
 export const Navbar = () => {
 
     return(
-        
+        <>
+        <nav className="navbar">
+            <li className="li">Home</li>
+            <li className="li">Search</li>
+            <li className="li">Library</li>
+            <li className="li">Favourites</li>
+        </nav>
+        </>
     )
 }
