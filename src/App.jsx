585bd782284f7a42996c4482eca50605f149425e
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-06-18 09:14:36 +0200

    imported Home and returned it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { Home } from "../presentation/Home";
 
-function App() {
+export default function App() {
 
   return (
     <>
+    <Home/>
     </>
   )
 }
 
-export default App
