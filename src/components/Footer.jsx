5675bc2bc3000c6010366a79347557a535574265
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-06-21 16:22:28 +0200

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+
+    return (
+        <footer className="footer">
+            &#169; 2024 PodPulse | All rights reserved. | Nomusa Mtshali 
+        </footer>
+    )
+}
