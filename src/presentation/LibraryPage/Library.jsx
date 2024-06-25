91a788d452d146a7ddbc93ed650d2c5b86c0aeaa
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-06-25 16:18:22 +0200

    netlify deploy debug

--- src/presentation/LibraryPage/Library.jsx
+++ src/presentation/LibraryPage/Library.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchPodcasts } from "../../services/api";
 import { Link, useSearchParams } from "react-router-dom";
-import { genreInfo } from "../../services/genres";
+import { genreInfo } from "../../services/genres.js";
 import "./Library.css";
 
 export const Library = () => {
